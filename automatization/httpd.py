db49b23ae44ce6c58d870a9e1d5c2b89d61e34c8
null <askill@pm.me>
2021-06-30 16:27:47 +0300

    Переписана обработка запросов с использованием пула процессов

--- automatization/httpd.py
+++ automatization/httpd.py
@@ -1,131 +1,181 @@
+
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import argparse
-import asyncio
 import logging
 import os
-import platform
-from utils import get_http_code_status, get_http_timestamp, get_mime_type, get_uri_path
+import signal
+import socket
+
+from multiprocessing import Pool
+from utils import (get_http_code_status, get_http_timestamp,
+                   get_mime_type, get_uri_path)
 
 
 class HttpServer(object):
     def __init__(self, host, port, doc_root, workers_num):
+        self.host = host
+        self.port = port
         self.workers_num = workers_num
-        self._queue = asyncio.Queue()
-        self._loop = asyncio.get_event_loop()
-        reuse_port = None if platform.system() == "Windows" else True
-        self._server_coro = asyncio.start_server(self._handle_connection,
-                                                 host=host, port=port,
-                                                 reuse_port=reuse_port)
         self.doc_root = doc_root
 
     def start(self):
-        self._loop.run_until_complete(self._start_workers())
-        self._server = self._loop.run_until_complete(self._server_coro)
-        logging.info(f'Listening on {self._server.sockets[0].getsockname()}')
-        self._loop.run_forever()
+        self.pool = Pool(
+                processes=self.workers_num,
+                initializer=HttpServer._worker_init
+        )
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(self.workers_num)
+
+        logging.info(f'Start server on {self.socket.getsockname()}')
+
+        while True:
+            conn, addr = self.socket.accept()
+            logging.info(f'Accept connection from {addr}')
+            self.pool.apply_async(HttpServer._worker, (conn, self.doc_root))
 
     def stop(self):
-        self._loop.run_until_complete(self._stop_workers())
-        self._server.close()
-        self._loop.run_until_complete(self._server.wait_closed())
-        self._loop.close()
+        logging.info('Stop server')
+        self.socket.close()
+        self.pool.close()
+        self.pool.join()
 
-    async def _start_workers(self):
-        self._workers = []
-        for _ in range(self.workers_num):
-            worker = asyncio.create_task(self._worker())
-            self._workers.append(worker)
+    @staticmethod
+    def _worker_init():
+        """ Отключаем обработку Ctrl-C в дочерних процессах.
+            Нажатие перехватит главный процесс и завершит
+            дочерние.
+        """
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-    async def _stop_workers(self):
-        for worker in self._workers:
-            worker.cancel()
+    @staticmethod
+    def _worker(conn, doc_root):
+        """ Получаем сокет для работы с клиентом. Читаем данные
+            запроса, вытаскиваем из тип запроса и URL. Передаем
+            обработку в _handle_request().
+        """
+        try:
+            request = HttpServer._get_request(conn)
+            if not request:
+                logging.info('Unexpected socket close')
+                return
+            params = request[0].split()
+            if len(params) != 3:
+                logging.error(f'Invalid request format')
+                conn.sendall(HttpServer._response_header(400))
+                return
+            rtype = params[0].decode('utf-8')
+            uri = get_uri_path(params[1].decode('utf-8'))
+            HttpServer._handle_request(rtype, uri, conn, doc_root)
+        except Exception as ex:
+            logging.exception(ex)
+        finally:
+            conn.shutdown(1)
+            conn.close()
 
-    async def _worker(self):
+    @staticmethod
+    def _get_request(conn):
+        """ Читаем запрос построчно до пустой строки или EOF.
+            Возвращаем запрос, как список строк.
+        """
+        request = [b'']
         while True:
-            (reader, writer) = await self._queue.get()
-
-            request = await self._read_request(reader)
-
-            if request:
-                start_str = request[0].split()
-                if len(start_str) >= 2:
-                    rtype = start_str[0]
-                    uri = get_uri_path(start_str[1])
-
-                    self._handle_request(rtype, uri, writer)
+            # TODO: timeout?
+            buf = conn.recv(1024)
+            if not buf:
+                return []
+            for line in buf.split(b'\n'):
+                if not line:
+                    continue
+                if request[-1].endswith(b'\r'):
+                    request.append(line)
                 else:
-                    logging.error(f'Invalid request format')
-                    writer.write(self._response_header(400))
-            else:
-                logging.error(f'Empty request')
-                writer.write(self._response_header(400))
-
-            writer.close()
-            self._queue.task_done()
-
-    async def _handle_connection(self, reader, writer):
-        peername = writer.get_extra_info('peername')
-        logging.info('Accepted connection from {}'.format(peername))
-
-        await self._queue.put((reader, writer))
-
-    async def _read_request(self, reader):
-        request = []
-        # Read request line by line until an empty one.
-        while True:
-            try:
-                data = await asyncio.wait_for(reader.readline(), timeout=10.0)
-                data = data.decode("utf-8").rstrip()
-                if not data:
-                    break
-                request.append(data)
-            except Exception:
-                return None
-
+                    request[-1] += line
+            if request[-1] == b'\r':
+                break
         return request
 
-    def _handle_request(self, rtype, uri, writer):
-        if rtype == "GET" or rtype == "HEAD":
-            logging.info(f'Handle {rtype} {uri}')
-            path = self._get_path(uri)
-            if path:
-                (_, ext) = os.path.splitext(path)
-                mime_type = get_mime_type(ext)
-
-                try:
-                    with open(path, 'rb') as file:
-                        content = file.read()
-                except Exception:
-                    logging.error(f'URI forbidden')
-                    writer.write(self._response_header(403))
-                    return
-
-                writer.write(
-                    self._response_header(200, mime_type, len(content)))
-                writer.write('\r\n'.encode("utf-8"))
-
-                if rtype == "GET":
-                    writer.write(content)
-            else:
-                logging.error(f'URI not found')
-                writer.write(self._response_header(404))
-        else:
+    def _handle_request(rtype, uri, conn, doc_root):
+        """ Обработка запроса. Получаем путь до файла на диске,
+            прорверяем его корректность. Возвращаем файл (GET)
+            или информацию о нем (HEAD).
+        """
+        if rtype != "GET" and rtype != "HEAD":
             logging.error(f'Unknown request {rtype} {uri}')
-            writer.write(self._response_header(405))
+            conn.sendall(HttpServer._response_header(405))
+            return
 
-    def _get_path(self, uri):
-        path = os.path.join(self.doc_root, uri.lstrip('/'))
+        logging.info(f'Handle {rtype} {uri}')
+        path = HttpServer._get_path(doc_root, uri)
+        if not path:
+            logging.error(f'URI not found')
+            conn.sendall(HttpServer._response_header(404))
+            return
+
+        logging.info(f'Requested {path}')
+
+        if not HttpServer._is_safe_path(doc_root, path):
+            logging.error(f'URI forbidden')
+            conn.sendall(HttpServer._response_header(403))
+            return
+
+        (_, ext) = os.path.splitext(path)
+        mime_type = get_mime_type(ext)
+
+        if rtype == "GET":
+            try:
+                with open(path, 'rb') as file:
+                    content = file.read()
+            except Exception:
+                logging.error(f'URI forbidden')
+                conn.sendall(HttpServer._response_header(403))
+                return
+
+            conn.sendall(
+                    HttpServer._response_header(200, mime_type, len(content))
+            )
+            conn.sendall(content)
+        else:
+            content_len = 0
+            try:
+                content_len = os.path.getsize(path)
+            except Exception:
+                logging.error(f'URI forbidden')
+                conn.sendall(HttpServer._response_header(403))
+                return
+
+            conn.sendall(
+                    HttpServer._response_header(200, mime_type, content_len)
+            )
+
+    @staticmethod
+    def _is_safe_path(doc_root, path):
+        """ Проверяем, что путь не выходит за границы 'корневой'
+            директории.
+        """
+        common_path = os.path.commonpath([doc_root, path])
+        return common_path == doc_root
+
+    @staticmethod
+    def _get_path(doc_root, uri):
+        """ Возвращаем путь до файла на диске, соответствующий
+            переданному URI.
+        """
+        path = os.path.join(doc_root, uri.lstrip('/'))
 
         if os.path.isdir(path):
             path = os.path.join(path, "index.html")
 
         if os.path.isfile(path):
-            return path
+            return os.path.realpath(path)
         else:
             return None
 
-    def _response_header(self, http_code, mime_type=None, content_length=None):
+    @staticmethod
+    def _response_header(http_code, mime_type=None, content_length=None):
+        """ Возвращаем строку - ответ сервера на запрос клиента. """
         response = (
             f"HTTP/1.1 {http_code} {get_http_code_status(http_code)}\r\n"
             f"Date: {get_http_timestamp()}\r\n"
@@ -137,6 +187,9 @@ class HttpServer(object):
             response += "Content-Type: {}\r\n".format(mime_type)
         if content_length:
             response += "Content-Length: {}\r\n".format(content_length)
+        else:
+            response += "Content-Length: 0\r\n"
+        response += "\r\n"
 
         return response.encode("utf-8")
 
